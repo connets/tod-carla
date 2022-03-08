bf250eff6e1ea6e5818dcb32c89241c4d8aa3d16
jaivra <jaivra.info@gmail.com>
2022-03-08 14:30:00 +0100

    working context

--- tests/test_context.py
+++ tests/test_context.py
@@ -1,8 +1,9 @@
 import os
 import sys
+
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src/')))
 
-from unittest.mock import MagicMock, PropertyMock
+from unittest.mock import MagicMock, PropertyMock, call, Mock
 from src.network_tele_world import NetworkTeleWorld
 from src.tele_context import TeleContext
 
@@ -34,7 +35,12 @@ def test_time_pass():
     assert tele_context.current_timestamp == tele_context.end_timestamp - tele_context.time_step
 
 
-    # event = create_empty_event(2 * 1000)
-    # world = NetworkTeleWorld()
-    # context.exec_next_event()
-    # assert context.simulation_time == event.time
+def test_world_is_called():
+    tele_context = TeleContext(100, 150, 10)
+    world = MagicMock()
+    world.proceed = MagicMock()
+
+    tele_context.add_world(world)
+    tele_context.start()
+
+    world.proceed.assert_has_calls([call(100), call(110), call(120), call(130), call(140)])
