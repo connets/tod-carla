25329aa079116884bc8436b5b119ea3d1fa41e38
jaivra <jaivra.info@gmail.com>
2022-09-20 10:52:24 +0200

    added complete trace of a simulation

--- sample/carla/connection.py
+++ sample/carla/connection.py
@@ -1,31 +1,33 @@
-import zerorpc
 import carla
 import logging
-
-host = '172.16.1.249'
-counts = 10
-i = 1
-retry = 4
-while i <= 10:
-    c = zerorpc.Client()
-    c.connect(f"tcp://{host}:4242")
-    c.reload_simulator()
-    r = 0
-    sim_world = None
-    while sim_world is None and r < retry:
-        try:
-            print(f'trying {i}, {r}...\n')
-            
-            client = carla.Client(host, 2000)
-            client.set_timeout(10.0)
-            sim_world = client.load_world("Town01_Opt")
-        except RuntimeError as e:
-            r += 1
-
-    if sim_world is None:
-        raise RuntimeError(f"** ERROR ** {i}")
-    print(f'** {i} DONE **\n\n')
-    i += 1
+#
+# host = '172.16.1.249'
+# counts = 10
+# i = 1
+# retry = 4
+# while i <= 10:
+#     c = zerorpc.Client()
+#     c.connect(f"tcp://{host}:4242")
+#     c.reload_simulator()
+#     r = 0
+#     sim_world = None
+#     while sim_world is None and r < retry:
+#         try:
+#             print(f'trying {i}, {r}...\n')
+#
+#             client = carla.Client(host, 2000)
+#             client.set_timeout(10.0)
+#             sim_world = client.load_world("Town01_Opt")
+#         except RuntimeError as e:
+#             r += 1
+#
+#     if sim_world is None:
+#         raise RuntimeError(f"** ERROR ** {i}")
+#     print(f'** {i} DONE **\n\n')
+#     i += 1
+#
     
-    
 
+client = carla.Client("ubiquity", 3000)
+client.set_timeout(15.0)
+traffic_manager = client.get_trafficmanager()
