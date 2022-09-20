29d360995ef44ca439fccdfb3302fd0a41d67092
jaivra <jaivra.info@gmail.com>
2022-09-20 11:31:45 +0200

    added redirect for stdout and stderr

--- src/EnvironmentHandler.py
+++ src/EnvironmentHandler.py
@@ -27,6 +27,20 @@ class CarlaTimeoutError(RuntimeError):
 
 
 class EnvironmentHandler:
+    class Logger(object):
+        def __init__(self, log_file_path):
+            self.terminal = sys.stdout
+            self.log = open(log_file_path, "a")
+
+        def write(self, message):
+            self.terminal.write(message)
+            self.log.write(message)
+
+        def flush(self):
+            # this flush method is needed for python 3 compatibility.
+            # this handles the flush command by doing nothing.
+            # you might want to specify some extra behavior here.
+            pass
 
     def __init__(self, world_configuration):
         self.tele_configuration = TeleConfiguration.instance
@@ -53,7 +67,7 @@ class EnvironmentHandler:
         if os.path.exists(self._simulation_out_dir):
             shutil.rmtree(self._simulation_out_dir)
         os.makedirs(self._simulation_out_dir)
-
+        sys.stdout = sys.stderr = self.Logger(self._simulation_out_dir + 'log.txt')
         self.tele_configuration.save_config(self._simulation_out_dir + 'configuration.yaml')
 
         self.clock = pygame.time.Clock()
